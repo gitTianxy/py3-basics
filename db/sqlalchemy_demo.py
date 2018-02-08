61ba3981a5175f8fa9d6a6172194b7acfa78ae9c
gitTianxy <txy_learning@163.com>
2018-02-08 14:24:28 +0800

    add: sqlalchemy demo

--- db/sqlalchemy_demo.py
+++ db/sqlalchemy_demo.py
@@ -2,25 +2,46 @@
 """
 SQLAlchemy: python中最有名的ORM框架
 """
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import Column, String, Integer, create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import exists, update
+from contextlib import closing
+
+# 创建对象的基类:
+Base = declarative_base()
+
+
+# 定义User对象:
+class User(Base):
+    # 表的名字:
+    __tablename__ = 'user'
+
+    # 表的结构:
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(20), unique=True, nullable=False)
+    sex = Column(String(10), nullable=False)
+    age = Column(Integer)
+    password = Column(String(20), nullable=False, default='1234')
+
+    def __repr__(self):
+        return f"User({self.name},{self.sex},{self.age},{self.password})"
 
 
 class MysqlDemo:
+    """
+    TODO
+    """
+
     def __init__(self):
-        # 创建对象的基类:
-        Base = declarative_base()
-
-        # 定义User对象:
-        class User(Base):
-            # 表的名字:
-            __tablename__ = 'user'
-
-            # 表的结构:
-            id = Column(String(20), primary_key=True)
-            name = Column(String(20))
-
+        """
+        init db connection settings
+        url for diff db-drivers:
+            mysql+mysqldb://<user>:<password>@<host>[:<port>]/<dbname>
+            mysql+pymysql://<username>:<password>@<host>/<dbname>[?<options>]
+            mysql+mysqlconnector://<user>:<password>@<host>[:<port>]/<dbname>
+        """
         # 初始化数据库连接:
         engine = create_engine('mysql+mysqlconnector://root:password@localhost:3306/test')
         # 创建DBSession类型:
@@ -41,45 +62,116 @@ class MysqlDemo:
 
 class SqliteDemo:
     def __init__(self):
-        # 创建对象的基类:
-        Base = declarative_base()
-
-        # 定义User对象:
-        class User(Base):
-            # 表的名字:
-            __tablename__ = 'user'
-
-            # 表的结构:
-            id = Column(String(20), primary_key=True)
-            name = Column(String(20))
-
+        """
+        init db connection settings
+        """
         # 初始化数据库连接:
-        engine = create_engine('sqlite:///./sqlalchemy.db')
+        self.eg = create_engine('sqlite:///./sqlite_demo.db')
+        if self.eg.dialect.has_table(self.eg, User.__tablename__):
+            self.drop_tbl()
+        Base.metadata.create_all(self.eg)
         # 创建DBSession类型:
-        DBSession = sessionmaker(bind=engine)
+        self.DBSession = sessionmaker(bind=self.eg)
 
-    def create(self):
-        pass
+    def create(self, u):
+        print("add", u)
+        with closing(self.DBSession()) as s:
+            if self.exists(u):
+                # raise RuntimeWarning(f"user named {u.name} already exists.")
+                print(f"!!!user named '{u.name}' already exists.")
+                return
+            s.add(u)
+            s.commit()
 
-    def retrieve(self):
-        pass
+    def add_ulist(self, ulist):
+        print("add user list", ulist)
+        with closing(self.DBSession()) as s:
+            s.add_all(ulist)
+            s.commit()
 
-    def update(self):
-        pass
+    def retrieve(self, name):
+        print("get by name:", name)
+        with closing(self.DBSession()) as s:
+            try:
+                return s.query(User).filter(User.name == name).one()
+            except NoResultFound:
+                return None
+            except:
+                raise
 
-    def delete(self):
-        pass
+    def get_ulist(self, offset=0, limit=5):
+        print(f"get user list. offset={offset}, limit={limit}")
+        with closing(self.DBSession()) as s:
+            return list(s.query(User).offset(offset).limit(limit))
+
+    def update(self, u):
+        print("update", u)
+        with closing(self.DBSession()) as s:
+            # method A
+            # s.query(User).filter(User.name == u.name).update({
+            #     "sex": u.sex,
+            #     "age": u.age,
+            #     "password": u.password
+            # })
+            # method B
+            # stmt = update(User).where(User.name == u.name).values(
+            #     sex=u.sex, age=u.age, password=u.password
+            # )
+            # s.execute(stmt)
+            # method C(orm styled)
+            for o in s.query(User).filter(User.name == u.name):
+                o.sex = u.sex
+                o.age = u.age
+                o.password = u.password
+            s.commit()
+
+    def delete(self, name):
+        with closing(self.DBSession()) as s:
+            s.query(User).filter(User.name == name).delete()
+            s.commit()
+
+    def exec(self, sql, param):
+        print("execute", sql)
+        with closing(self.DBSession()) as s:
+            res = s.execute(sql, param)
+            for r in res:
+                print(r)
+
+    def count(self, sex):
+        print("count", sex)
+        with closing(self.DBSession()) as s:
+            return s.query(User).filter(User.sex == sex).count()
+
+    def exists(self, u):
+        cond = exists().where(User.name == u.name)
+        with closing(self.DBSession()) as s:
+            return s.query(cond).scalar()
+
+    def drop_tbl(self):
+        User.__table__.drop(self.eg)
 
 
 if __name__ == '__main__':
-    mysql = MysqlDemo()
-    mysql.create()
-    mysql.retrieve()
-    mysql.update()
-    mysql.delete()
+    # mysql = MysqlDemo()
+    # mysql.create()
+    # mysql.retrieve()
+    # mysql.update()
+    # mysql.delete()
 
+    print("---sqlite demo")
     sqlite = SqliteDemo()
-    sqlite.create()
-    sqlite.retrieve()
-    sqlite.update()
-    sqlite.delete()
+    kevin = User(name='kevin', sex='male', age=18, password='1234')
+    sqlite.create(kevin)
+    ulist = [
+        User(name='zhang1', sex='male', age=18, password='1234'),
+        User(name='zhang2', sex='female', age=19),
+        User(name='zhang3', sex='female', password='1234'),
+    ]
+    sqlite.add_ulist(ulist)
+    print(sqlite.retrieve('kevin'))
+    sqlite.delete('zhang2')
+    kevin_new = User(name='kevin', sex='male', age=30, password='2018')
+    sqlite.update(kevin_new)
+    print(sqlite.get_ulist())
+    print(sqlite.count('female'))
+    sqlite.exec(sql='select * from user where name=:name', param={'name': 'kevin'})
