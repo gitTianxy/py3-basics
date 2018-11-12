9a14b6e20a92dcb8aa1628d7e5c3e0cfbe69db60
kevintian <kevintian@pptv.com>
2018-11-12 11:32:05 +0800

    update

--- util/log_utils.py
+++ util/log_utils.py
@@ -10,9 +10,12 @@ DEFAULT_FORMAT = "%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(mess
 MESSAGE_FORMAT = "%(message)s"
 DEFAULT_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
+MODE_APPEND = "a"
+MODE_WRITE = "w"
 
-def setup_logger(name, log_file, level=logging.INFO, fmt=DEFAULT_FORMAT, datefmt=DEFAULT_DATE_FORMAT):
-    handler = logging.FileHandler(log_file)
+
+def setup_logger(name, log_file, level=logging.INFO, fmt=DEFAULT_FORMAT, datefmt=DEFAULT_DATE_FORMAT, mode=MODE_APPEND):
+    handler = logging.FileHandler(filename=log_file, mode=mode)
     handler.setFormatter(logging.Formatter(fmt=fmt, datefmt=datefmt))
     logger = logging.getLogger(name)
     logger.setLevel(level)
@@ -20,7 +23,7 @@ def setup_logger(name, log_file, level=logging.INFO, fmt=DEFAULT_FORMAT, datefmt
     return logger
 
 
-def enable_stdout(level=logging.INFO, fmt=MESSAGE_FORMAT):
+def enable_stdout(level=logging.INFO, fmt=DEFAULT_FORMAT):
     console = logging.StreamHandler()
     console.setLevel(level)
     formatter = logging.Formatter(fmt)
@@ -30,9 +33,9 @@ def enable_stdout(level=logging.INFO, fmt=MESSAGE_FORMAT):
 
 if __name__ == "__main__":
     enable_stdout(LEVEL_DEBUG)
-    debug_logger = setup_logger("log_test_debug", "./log_test_debug.log", LEVEL_DEBUG)
-    info_logger = setup_logger("log_test_info", "./log_test_info.log", LEVEL_INFO)
-    err_logger = setup_logger("log_test_error", "./log_test_error.log", LEVEL_ERROR)
+    debug_logger = setup_logger("log_test_debug", "./log_test_debug.log", LEVEL_DEBUG, mode=MODE_WRITE)
+    info_logger = setup_logger("log_test_info", "./log_test_info.log", LEVEL_INFO, mode=MODE_WRITE)
+    err_logger = setup_logger("log_test_error", "./log_test_error.log", LEVEL_ERROR, mode=MODE_WRITE)
 
     for i in range(0, 10):
         debug_logger.debug("debug %s", i)
